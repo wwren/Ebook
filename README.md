1f9f4042a0ed4949ff2603b37c37bac612e5b3f1
wwren <rdding@student.unimelb.edu.au>
2023-08-06 20:31:50 +1000

    change translate endpoint to more versatile quick ask and ask anything

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Ebook
+
+[UX/UI project goals](https://ten-jellyfish-910.notion.site/UX-UI-93581a3b3d85415b90a9248ae4c47462?pvs=4)
+
+[API test result](https://ten-jellyfish-910.notion.site/API-Test-Result-3b14c7299bb042f2bf50c2ca2db8060d?pvs=4)
--- functions/index.js
+++ functions/index.js
@@ -9,7 +9,7 @@ const axios = require('axios');
 setGlobalOptions({ region: 'australia-southeast2' });
 const apiKey = defineSecret('TRANSLATE_GPT_API_KEY');
 
-exports.translate = onRequest({ secrets: [apiKey] }, async (req, res) => {
+exports.quickAsk = onRequest({ secrets: [apiKey] }, async (req, res) => {
   const API_KEY = apiKey.value();
   if (!API_KEY) {
     res.status(500).json({
@@ -21,16 +21,16 @@ exports.translate = onRequest({ secrets: [apiKey] }, async (req, res) => {
   }
 
   cors(req, res, async () => {
-    const sentence = req.body.sentence;
-    const language = req.body.language;
+    const { action, selectedSentence, language } = req.body;
+    const generatedContent = generatePromptForAction(action, language);
     try {
       const apiResponse = await axios.post(
         'https://api.openai.com/v1/chat/completions',
         {
           model: 'gpt-3.5-turbo',
           messages: [
-            { role: 'system', content: generatePromptForTranslation(language) },
-            { role: 'user', content: sentence },
+            { role: 'system', content: generatedContent },
+            { role: 'user', content: selectedSentence },
           ],
           temperature: 0.6,
         },
@@ -39,7 +39,7 @@ exports.translate = onRequest({ secrets: [apiKey] }, async (req, res) => {
         },
       );
       const completion = apiResponse.data.choices[0].message.content;
-      logger.info('apiResponse', completion);
+      logger.info('completion', completion);
       res.status(200).json({
         message: 'OpenAI API connection successful.',
         completion,
@@ -60,8 +60,83 @@ exports.translate = onRequest({ secrets: [apiKey] }, async (req, res) => {
   });
 });
 
+// Need to past down the conversation history
+exports.askAnything = onRequest({ secrets: [apiKey] }, async (req, res) => {
+  const API_KEY = apiKey.value();
+  if (!API_KEY) {
+    res.status(500).json({
+      error: {
+        message: 'OpenAI API key not configured, please follow instructions in README.md',
+      },
+    });
+    return;
+  }
+
+  cors(req, res, async () => {
+    const { history, newQuestion } = req.body;
+
+    try {
+      const apiResponse = await axios.post(
+        'https://api.openai.com/v1/chat/completions',
+        {
+          model: 'gpt-3.5-turbo',
+          messages: [...history, { role: 'user', content: newQuestion }],
+          temperature: 0.6,
+        },
+        {
+          headers: generateHeaders(API_KEY),
+        },
+      );
+      const completion = apiResponse.data.choices[0].message.content;
+      logger.info('completion', completion);
+      res.status(200).json({
+        message: 'OpenAI API connection successful.',
+        completion,
+      });
+    } catch (error) {
+      if (error.response) {
+        logger.error('error.response', error.response.status, error.response.data);
+        res.status(error.response.status).json(error.response.data);
+      } else {
+        logger.error(`Error with OpenAI API request: ${error.message}`);
+        res.status(500).json({
+          error: {
+            message: 'An error occurred during your request.',
+          },
+        });
+      }
+    }
+  });
+});
+
+function generatePromptForAction(action, language) {
+  let content;
+
+  switch (action) {
+    case 'translate':
+      content = generatePromptForTranslation(language);
+      break;
+    case 'define':
+      content = generatePromptForDefination(language);
+      break;
+    case 'summarise':
+      content = generatePromptForSummarisation(language);
+      break;
+  }
+
+  return content;
+}
+
 function generatePromptForTranslation(language) {
-  return `You will be provided with a sentence in English, and your task is to translate it into ${language}.`;
+  return `You will be provided with sentences, and your task is to translate them into ${language}.`;
+}
+
+function generatePromptForDefination(language) {
+  return `You will be provided with a work or a phrase, and your task is to provide defination and usage for this word in ${language}.`;
+}
+
+function generatePromptForSummarisation(language) {
+  return `You will be provided with paragraphs, and your task is to give a summary in ${language}.`;
 }
 
 function generateHeaders(apiKey) {
